3b6ba7503538632b6d323fbd99416e87374bce98
moonsoo.kwon <moonsoo.kwon@seeroo.co.kr>
2026-05-22 13:51:06 +0900

    feat) secrets manager 실습

--- app.js
+++ app.js
@@ -1,8 +1,49 @@
 const express = require('express');
 const app = express();
 
+// Use this code snippet in your app.
+// If you need more information about configurations or implementing the sample code, visit the AWS docs:
+// https://aws.amazon.com/developers/getting-started/nodejs/
+
+// Load the AWS SDK
+var AWS = require('aws-sdk'),
+    endpoint = "",
+    region = "",
+    secretName = "",
+    secret,
+    binarySecretData;
+
+// Create a Secrets Manager client
+var client = new AWS.SecretsManager({
+    endpoint: endpoint,
+    region: region,
+    accessKeyId: '', // 본인의 ID를 입력
+    secretAccessKey: '' // 본인의 키를 입력
+});
+
 app.get('/', (req, res) => {
- res.send('CodeDeploy로 배포된 AWS exercise의 두 번째 A project 버전입니다.');
+  client.getSecretValue({ SecretId: secretName }, function (err, data) {
+    if (err) {
+      if (err.code === 'ResourceNotFoundException')
+        console.log("The requested secret " + secretName + " was not found");
+      else if (err.code === 'InvalidRequestException')
+        console.log("The request was invalid due to: " + err.message);
+      else if (err.code === 'InvalidParameterException')
+        console.log("The request had invalid params: " + err.message);
+    }
+    else {
+      // Decrypted secret using the associated KMS CMK
+      // Depending on whether the secret was a string or binary, one of these fields will be populated
+      if (data.SecretString !== "") {
+        secret = JSON.parse(data.SecretString);
+      } else {
+        binarySecretData = data.SecretBinary;
+      }
+    }
+
+    res.send(`SecretsManager로 실행되는 AWS exercise의 A project입니다.<br />
+- sercetKey: ${secret.sercetKey}<br />`);
+  });
 });
 
 app.listen(3000, () => {
